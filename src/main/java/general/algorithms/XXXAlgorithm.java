d0aa3cfa5225df553ea2b5f2cc7bafb58b6aaef6
Martonca Daniel <martonca.daniel@yahoo.com>
2021-10-19 13:37:57 +0300

    Added OFB.

--- src/main/java/general/algorithms/XXXAlgorithm.java
+++ src/main/java/general/algorithms/XXXAlgorithm.java
@@ -108,6 +108,69 @@ public class XXXAlgorithm implements EncryptionAlgorithmAES {
         return c.doFinal(block);
     }
 
+    //OFB
+    public List<String> encryptOFB(String plainText, SecretKey key, IvParameterSpec iv) {
+        List<String> cipherTextList = new LinkedList<>();
+        try {
+            var blocksList = getStringAsByteBlocksList(plainText, BlockSizeAES.BLOCK_SIZE_OFB);
+
+            var xorBlock = iv.getIV();
+            var encryptionValue = encryptBlockOFB(xorBlock, key, iv);
+            var xorValue = encryptionValue;
+
+            var plainTextAsByte = convertStringToByte(convertByteToString(blocksList.get(0)));
+            var value = xor(encryptionValue, plainTextAsByte);
+            cipherTextList.add(convertByteToString(value));
+
+            for (int i = 1; i < blocksList.size(); i++) {
+                encryptionValue = encryptBlockOFB(xorValue, key, iv);
+                xorValue = encryptionValue;
+
+                plainTextAsByte = convertStringToByte(convertByteToString(blocksList.get(i)));
+                value = xor(encryptionValue, plainTextAsByte);
+                cipherTextList.add(convertByteToString(value));
+            }
+
+        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+        return cipherTextList;
+    }
+
+    public String decryptOFB(List<String> cipherTextList, SecretKey key, IvParameterSpec iv) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            var ivByteArray = iv.getIV();
+            var decryptedBlock = decryptBlockOFB(ivByteArray, key, iv);
+
+            var xorValue = decryptedBlock;
+            stringBuilder.append(convertByteToString(xor(decryptedBlock, convertStringToByte(cipherTextList.get(0)))));
+
+            for (int i = 1; i < cipherTextList.size(); i++) {
+                decryptedBlock = decryptBlockOFB(xorValue, key, iv);
+                xorValue = decryptedBlock;
+
+                stringBuilder.append(convertByteToString(xor(decryptedBlock, convertStringToByte(cipherTextList.get(i)))));
+            }
+        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private byte[] encryptBlockOFB(byte[] block, SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        Cipher c = Cipher.getInstance("AES/OFB32/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, key, iv);
+        return c.doFinal(block);
+    }
+
+    private byte[] decryptBlockOFB(byte[] block, SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        Cipher c = Cipher.getInstance("AES/OFB32/PKCS5Padding");
+        c.init(Cipher.DECRYPT_MODE, key, iv);
+        return c.doFinal(block);
+    }
+
 
     //CFB
     public List<String> encryptCFB(String plainText, SecretKey key, IvParameterSpec iv) {
@@ -156,52 +219,5 @@ public class XXXAlgorithm implements EncryptionAlgorithmAES {
         return c.doFinal(block);
     }
 
-
-    //OFB
-    public List<String> encryptOFB(String plainText, SecretKey key, IvParameterSpec iv) {
-        List<String> cipherTextList = new LinkedList<>();
-        try {
-            //divide string into blocks of fixed size (128) for ECB
-            var blocksList = getStringAsByteBlocksList(plainText, BlockSizeAES.BLOCK_SIZE_OFB);
-
-            for (var block : blocksList) {
-                var encryptedBlock = encryptBlockOFB(block, key, iv);  //encrypt each block with a key
-                cipherTextList.add(convertByteToString(encryptedBlock));    //add it to the list
-            }
-        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        }
-        return cipherTextList;
-    }
-
-    public String decryptOFB(List<String> cipherTextList, SecretKey key, IvParameterSpec iv) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            for (var encryptedBlockAsString : cipherTextList) {
-                var encryptedBlock = convertStringToByte(encryptedBlockAsString);
-
-                var decryptedBlock = decryptBlockOFB(encryptedBlock, key, iv);
-                decryptedBlock = removePaddingIfNecessary(decryptedBlock, BlockSizeAES.BLOCK_SIZE_OFB);
-
-                stringBuilder.append(convertByteToString(decryptedBlock));
-            }
-        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        }
-
-        return stringBuilder.toString();
-    }
-
-    private byte[] encryptBlockOFB(byte[] block, SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        Cipher c = Cipher.getInstance("AES/OFB32/PKCS5Padding");
-        c.init(Cipher.ENCRYPT_MODE, key, iv);
-        return c.doFinal(block);
-    }
-
-    private byte[] decryptBlockOFB(byte[] block, SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        Cipher c = Cipher.getInstance("AES/OFB32/PKCS5Padding");
-        c.init(Cipher.DECRYPT_MODE, key, iv);
-        return c.doFinal(block);
-    }
 
 }
